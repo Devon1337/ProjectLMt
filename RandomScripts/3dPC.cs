210bd4d41d90f385ba7f15ffc15067389de556b3
Devon <Sanitygamers@outlook.com>
2016-12-15 08:25:53 -0600

    Create 3dPC.cs
    Adds Third Person Player Controller to the regular use asset

---
+++ RandomScripts/3dPC.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Generic;
+
+class 3dPC() {
+
+  // Public Movement/Rotation of an object; Defined in the engine;
+  public Transform playerObject, playerCamera;
+  // Speed of the camera and player
+  public float speed, turnSpeed;
+  // Player Direction
+  Vector3 forceDirection;
+  // PlayerController
+  GameObject player;
+
+  // Every Given Update
+  void FixedUpdate() {
+    transform.position = playerObject.position;
+    if(Input.GetAxis("Vertical")) {
+      MovePlayer();
+    }
+  
+  }
+  
+  void MovePlayer() {
+    // Moves the Player And the PlayerCamera To assign a location Around a Centralized Object.
+    forceDirection = playerCamera.transform.forward;
+    forceDirection = new Vector3(forceDirection.x, 0, forceDirection.z);
+    
+    // Checks for a rigidbody to be applied to a GameObject and places a direction force
+    playerController,GetComponent<Rigidbody>().AddForce(forceDirection.normalized * speed * (Input.GetAxis("Vertical")));
+  }
+
+}
